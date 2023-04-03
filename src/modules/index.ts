8a6b1615851a2eaa9a95493ff844f88ff15c9ee4
RobinGrit22 <112506404+RobinGrit22@users.noreply.github.com>
2023-04-03 15:58:10 +0200

    asdfsdf

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,19 +1,29 @@
-import { login } from "./firebase"
+import { login } from "./firebase";
 
+const formElement = document.querySelector('form');
 
-document.querySelector('form')?.addEventListener('submit', (event) => {
+if (formElement) {
+  formElement.addEventListener('submit', async (event) => {
     event.preventDefault();
     const email = (document.getElementById('email') as HTMLInputElement).value;
     const password = (document.getElementById('password') as HTMLInputElement).value;
     console.log(`Email: ${email}, Password: ${password}`);
-    login(email, password)
-      .then(() =>{
-       window.location.assign('./html/mainpage.html');
-    })
-    .catch((error) => {
+    
+    try {
+      console.log('Attempting login...');
+      await login(email, password);
+      console.log('Login successful, redirecting...');
+      window.location.assign('./html/mainpage.html');
+    } catch (error) {
+      console.error('Error during login:', error);
       alert('No account found. Please check your email and password.');
-    });
-});
+    }
+  });
+} else {
+  console.error('Form not found');
+}
+
+
   
 
 
